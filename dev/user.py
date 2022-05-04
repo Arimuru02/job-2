b8654e17ffdbb26d92fea7b6704661ba8fa9031d
Alex <bojenko02@mail.ru>
2022-05-04 10:22:24 +0300

    dev

---
+++ dev/user.py
@@ -0,0 +1,4 @@
+nameUser=input("Как вас зовут? ")
+ageUser=input("Сколько вам лет? ")
+cityUser=input("В каком городе вы живете? ")
+print("Вас зовут {0}, Вам {1} лет, Вы из города {2}".format(nameUser, ageUser, cityUser))
