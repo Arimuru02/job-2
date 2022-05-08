2384afa1669c1ba54a8360f3aa11729e64412c83
Alex Li <99331143+Arimuru02@users.noreply.github.com>
2022-05-08 16:26:12 +0300

    Delete dev directory

--- dev/user.py
+++
@@ -1,4 +0,0 @@
-nameUser=input("Как вас зовут? ")
-ageUser=input("Сколько вам лет? ")
-cityUser=input("В каком городе вы живете? ")
-print("Вас зовут {0}, Вам {1} лет, Вы из города {2}".format(nameUser, ageUser, cityUser))
