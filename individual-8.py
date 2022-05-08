6d9bb4cb972c441fed3138f6fd689f6dad0e9b84
Alex <bojenko02@mail.ru>
2022-05-08 16:00:14 +0300

    dev-1

---
+++ individual-8.py
@@ -0,0 +1,7 @@
+import math
+
+a=float(input("Длинна большего основания а: "))
+b=float(input("Длинна меньшего основания b: "))
+alpha=float(input("Угол при большем основании в градусах: "))
+alpha=alpha*math.pi/180
+print("Площадь трапеции равна ",(a**2-b**2)/4*math.tan(alpha))
